d61c0c51fa88c2bd6e7299cc9a7e476c9edd7d8e
YMJ1123 <88578309+YMJ1123@users.noreply.github.com>
2024-12-11 15:11:06 +0800

    Original utils

--- reverie/backend_server/utils.py
+++ reverie/backend_server/utils.py
@@ -5,13 +5,9 @@ os.environ["http_proxy"] = proxy
 os.environ["https_proxy"] = proxy
 
 # Copy and paste your OpenAI API Key
-# use Grok API
-openai_api_key = "xai-dHOWkj742Q85Zp5gexXOHRu32WkqzaFssq9U9R0iypqN3kVdEcSZ4jC7s48ioIostmUcGo5nDbVLkIIx"
+openai_api_key = "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"
 # Put your name
-key_owner = "" 
-
-# OpenAI API Key for embedding
-embedding_openai_api_key = "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"
+key_owner = "ymj"
 
 maze_assets_loc = "../../environment/frontend_server/static_dirs/assets"
 env_matrix = f"{maze_assets_loc}/the_ville/matrix"
