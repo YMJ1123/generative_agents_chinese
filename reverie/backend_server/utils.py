5cf9caca686bc064daa8929439566ae50b5cd098
Ying <b09611013@ntu.edu.tw>
2024-12-19 02:22:56 +0800

    1219

--- reverie/backend_server/utils.py
+++ reverie/backend_server/utils.py
@@ -5,7 +5,11 @@ os.environ["http_proxy"] = proxy
 os.environ["https_proxy"] = proxy
 
 # Copy and paste your OpenAI API Key
+<<<<<<< HEAD
 openai_api_key = "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"
+=======
+openai_api_key = ""
+>>>>>>> 767ac82a (1219)
 # Put your name
 key_owner = "ymj"
 
